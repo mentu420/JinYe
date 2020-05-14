d94ba467720c3e0d56136b75578b78dbe931b769
ljx23136138 <ljx23136138@gmail.com>
2020-05-14 23:27:47 +0800

    home界面添加锚点移动

--- src/components/layout/home/index.js
+++ src/components/layout/home/index.js
@@ -20,15 +20,30 @@ export default class Home extends Component {
         })
 
     }
-    onDownClick = () => {
-        console.log(1231646)
+    scrollToAnchor = (e) => {
+        // 找到锚点
+        let anchorElement = document.querySelectorAll('.home-item');
+        let clientHeight = document.body.clientHeight
+        let n = parseInt(e.pageY / clientHeight)
+        n = n == 0 ? 1 : n
+        if (n >= anchorElement.length) {
+            //滚动到footer
+            let homeElement = document.querySelector('.home-content')
+            let height = parseInt(window.getComputedStyle(homeElement).height)
+            window.scroll({ top: height + clientHeight, left: 0, behavior: 'smooth' });
+            return
+        }
+        // 如果对应id的锚点存在，就跳转到锚点
+        anchorElement[n].scrollIntoView({ block: 'start', behavior: 'smooth' })
+
     }
     render() {
         let { clientHeight } = this.state
         console.log(clientHeight)
+
         return (
             <div class="home-content">
-                <div class="home-item" >
+                <div class="home-item">
                     <Carousel>
                         <Carousel.Item>
                             <div class="banner">
@@ -37,7 +52,7 @@ export default class Home extends Component {
                         </Carousel.Item>
                     </Carousel>
                 </div>
-                <div class="home-item" >
+                <div class="home-item" style={{ minHeight: clientHeight }}>
                     <Container fluid>
                         <Row>
                             <Col lg={{ span: 5, offset: 1 }} md={12}>
@@ -63,7 +78,9 @@ export default class Home extends Component {
                             </Col>
                             <Col>
                                 <div class="item-product__btn">
-                                    <h3>熔喷布加热器</h3>
+                                    <div class="produt-right__title">
+                                        <h3>熔喷布加热器</h3>
+                                    </div>
                                     <Button variant="light">了解详情</Button>
                                 </div>
                                 <div class="item-product__bg">
@@ -73,18 +90,18 @@ export default class Home extends Component {
                         </Row>
                     </Container>
                 </div>
-                <div class="home-item" >
+                <div class="home-item" style={{ minHeight: clientHeight }}>
                     <div>
                         <p>PRODUCT USE</p>
                         <p>产品用途</p>
                     </div>
                     <div></div>
                 </div>
-                <div class="home-item" >
+                <div class="home-item" style={{ minHeight: clientHeight }}>
                     <Container fluid>
                         <Row>
                             <Col>
-                                <div>文案</div>
+                                <div>4文案</div>
                             </Col>
                             <Col>
                                 <div>图片</div>
@@ -92,11 +109,11 @@ export default class Home extends Component {
                         </Row>
                     </Container>
                 </div>
-                <div class="home-item" >
-                    <div>背景图</div>
+                <div class="home-item" style={{ minHeight: clientHeight }}>
+                    <div>5背景图</div>
                     <div>恒于心 专与质</div>
                 </div>
-                <div class="home-item" >
+                <div class="home-item" style={{ minHeight: clientHeight }}>
                     <div>
                         <p>NEWS INFORMATION</p>
                         <p>新闻动态</p>
@@ -114,7 +131,7 @@ export default class Home extends Component {
                         </Container>
                     </div>
                 </div>
-                <div class="home-nav__down" onClick={this.onDownClick}>
+                <div class="home-nav__down" onClick={(e) => this.scrollToAnchor(e)}>
                     <span class="iconfont icon-down"></span>
                 </div>
             </div>
